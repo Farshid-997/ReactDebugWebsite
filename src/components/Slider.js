70fc98ae41d65951fdef57385f614cc5e04c4c19
Farshid-997 <arnob.farshid@gmail.com>
2023-04-04 16:08:06 +0600

    fix in the slider components

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,32 +1,35 @@
 import { useEffect, useState } from 'react';
 
-// // Images
+// Images
 import bg01 from '../assets/images/bg01.jpg';
 import bg02 from '../assets/images/bg02.jpg';
 import bg03 from '../assets/images/bg03.jpg';
-let index = 0;
 
 export default function Slider() {
-  let [slide, setSlide] = useState(bg03);
-  // let [visibility, setVisibility] = useState('');
+  const [slide, setSlide] = useState(bg03);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    let delay = 7000;
-    // let speed = 1000;
-    let slides = [bg01, bg02, bg03];
+    const delay = 7000;
+    const slides = [bg01, bg02, bg03];
     const interval = setInterval(() => {
-      if (index >= slides.length) index = 0;
-      setSlide(slides[index])
-      index += 1
+      setSlide(slides[index]);
+      setIndex((index + 1) % slides.length);
     }, delay);
     return () => {
       clearInterval(interval);
-    }
-  }, [])
+    };
+  }, [index]);
 
   return (
     <div className="simpleslide100">
-      <div className="simpleslide100-item bg-img1" style={{ backgroundImage: `url(${slide})`, animation: 'fadeIn 7s infinite' }}></div>
+      <div
+        className="simpleslide100-item bg-img1"
+        style={{
+          backgroundImage: `url(${slide})`,
+          animation: 'fadeIn 7s infinite',
+        }}
+      ></div>
     </div>
   );
 }
